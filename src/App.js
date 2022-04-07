1d07510ce9da8cce28f777340d38a990e5dbcef4
wjtelliott <wjtelliott@gmail.com>
2022-04-06 22:14:23 -0500

    Added some comments

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import MoleContainer from './components/moleContainer';
 function App() {
 
 
+  // Player global score
   let [score, setScore] = useState(0);
 
 
   const createMoleHill = () => {
 
+    // Create our mole hills. Push 9 total containers to the App.
     let hills = [];
     for (let i = 0; i < 9; i++) {
       hills.push(
@@ -25,13 +27,14 @@ function App() {
         {hills}
       </div>
     )
-    
+
   }
 
 
   return (
     <div className="App">
 
+      {/* Display our score and hills */}
       <h1>Score: {score}</h1>
       {createMoleHill()}
 
--- src/components/mole.jsx
+++ src/components/mole.jsx
@@ -1,21 +1,30 @@
 import { useEffect, useState } from 'react';
-//import moleImg from '';
 
 function Mole(props) {
 
+    // Is this mole hiding?
     const [isHiding, setIsHiding] = useState(true);
 
     const moleClicked = () => {
+        // Guard clause. Is this mole visible?
         if (isHiding) return;
+
+        // Hide the mole, handle the parent click
         setIsHiding(true);
         props.handleClick();
     };
 
     useEffect(() => {
+
+        // Get random seconds between .4 - 1.2
         const randSeconds = Math.floor(Math.random() * 1200) + 400;
+
+        // Set a time for the random seconds, and flip/flop the mole's hiding value
         const timer = setTimeout(() => setIsHiding(!isHiding), randSeconds);
 
         return () => clearTimeout(timer);
+
+        // This effect will 'call itself' immediately after the internal timer finishes
     }, [isHiding]);
 
     return (
